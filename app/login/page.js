6b0a680e1abd58b4aee513238ed31f301a3fc523
Minhaz <minhazpro1@gmail.com>
2024-12-16 10:40:07 +0600

    solved error

--- app/login/page.js
+++ app/login/page.js
@@ -1,11 +1,11 @@
 "use client";
-import Login from "@/components/auth/Login";
+ 
 import React from "react";
 
 const Page = () => {
   return (
     <div className="container mx-auto px-8 md:px-0">
-      {/* <Login /> */}
+       
     </div>
   );
 };
--- app/register/page.js
+++ app/register/page.js
@@ -1,11 +1,10 @@
 "use client";
-import Register from "@/components/auth/Register";
 import React from "react";
 
 const Page = () => {
   return (
     <div className="container mx-auto px-8 md:px-0">
-      {/* <Register /> */}
+      
     </div>
   );
 };
